4990474d1a2bd4bab8e224c2adfd4a8618bb151d
vijaymane4104 <vijaymane4104@gmail.com>
2026-05-31 23:44:50 +0530

    EP-02 and EP-03

--- App.js
+++ App.js
@@ -7,15 +7,15 @@ const parent = React.createElement(
     id: "Parent",
   },
   [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", { id: "hading" }, "I am a header"),
-      React.createElement("h1", { id: "hading" }, "I am a header2"),
+    React.createElement("div", { id: "child1" }, [
+      React.createElement("h1", { id: "heading" }, "This is a Namaste React"),
+      React.createElement("h1", { id: "heading" }, "I am a header2"),
     ]),
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", { id: "hading" }, "I am a header"),
-      React.createElement("h1", { id: "hading" }, "I am a header2"),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", { id: "heading" }, "I am a header"),
+      React.createElement("h1", { id: "heading" }, "I am a header2"),
     ]),
-  ]
+  ],
 );
 
 const heading = React.createElement(
@@ -23,8 +23,21 @@ const heading = React.createElement(
   {
     id: "heading",
   },
-  "Hello world from React !"
+  "Hello world from React !",
 );
+
+const JsxHeading = () => <h1 id="heading">This is Namste React from JSX</h1>;
+
+const HeadingComponent = () => (
+  <div id="Container">
+    {JsxHeading()}
+    <JsxHeading />
+    <JsxHeading></JsxHeading>
+    <h1>Namste React funtional Component</h1>
+  </div>
+);
+
+console.log(JsxHeading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
